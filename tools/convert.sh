a060d821094cbe24a837ac0cb4e8bddae2c7aae2
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-11-25 13:45:21 -0600

    Publish changes to gh-pages automatically using Travis CI.

---
+++ tools/convert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Environment variables are defined in the Travis project settings.
+set -ev
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  cd $TRAVIS_BUILD_DIR
+  # Clone gh-pages branch into _site/
+  git clone -q -b gh-pages https://$GH_TOKEN@github.com/agrueneberg/ankidroiddocs.git _site/
+  # Convert *.txt files to HTML5 and write them into _site/
+  asciidoctor *.txt -D _site/
+  # Push changes to gh-pages branch
+  cd _site/
+  git config user.email $GIT_EMAIL
+  git config user.name "$GIT_NAME"
+  git add -A .
+  git commit -m "Update site: Travis #$TRAVIS_BUILD_NUMBER."
+  git push -q origin gh-pages
+fi
