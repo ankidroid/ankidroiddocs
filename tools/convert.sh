747e242b828fa61c3497c68cf26540c81c78ede7
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-11-25 21:49:00 -0600

    Update link to repo.

--- tools/convert.sh
+++ tools/convert.sh
@@ -4,7 +4,7 @@ set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   cd $TRAVIS_BUILD_DIR
   # Clone gh-pages branch into _site/
-  git clone -q -b gh-pages https://$GH_TOKEN@github.com/agrueneberg/ankidroiddocs.git _site/
+  git clone -q -b gh-pages https://$GH_TOKEN@github.com/ankidroid/ankidroiddocs.git _site/
   # Convert *.txt files to HTML5 and write them into _site/
   asciidoctor *.txt -D _site/
   # Push changes to gh-pages branch
