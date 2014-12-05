f4244d143fe204b10547cb58e2580fe75b15e7c0
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2014-12-05 17:04:06 -0600

    Update convert script to deploy to remote server.

--- tools/convert.sh
+++ tools/convert.sh
@@ -3,15 +3,18 @@
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   cd $TRAVIS_BUILD_DIR
-  # Clone gh-pages branch into _site/
-  git clone -q -b gh-pages https://$GH_TOKEN@github.com/ankidroid/ankidroiddocs.git _site/
+  # Add private key to agent
+  chmod 600 tools/id_rsa
+  ssh-add tools/id_rsa
+  # Clone remote branch into _site/
+  git clone travis@ankidroid.org:/home/travis/ankidroiddocs.git _site/
   # Convert *.txt files to HTML5 and write them into _site/
   asciidoctor *.txt -D _site/
-  # Push changes to gh-pages branch
+  # Push changes to remote branch
   cd _site/
-  git config user.email $GIT_EMAIL
-  git config user.name "$GIT_NAME"
+  git config user.name "Travis CI"
+  git config user.email travis@ankidroid.org
   git add -A .
   git commit -m "Update site (Travis Build $TRAVIS_BUILD_NUMBER)."
-  git push -q origin gh-pages
+  git push origin master
 fi
